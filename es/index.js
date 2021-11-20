e1baf47a3a301ae4d61993c691a43b7bddd0dfe3
yueluo <yueluo.yang@qq.com>
2021-11-21 07:51:47 +0800

    perf: 修改目录结构

--- es/index.js
+++ es/index.js
@@ -1,2 +1,5 @@
-export { default as Foo } from './Foo';
-export { default as TreeTransfer, normalizeTreeData } from './TreeTransfer';
+export { default as Foo } from './components/Foo';
+export {
+  default as TreeTransfer,
+  normalizeTreeData,
+} from './components/TreeTransfer';
